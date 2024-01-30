9c2b84fc2f7574ee003432c82250736d52b87ff9
tjdwns335 <http://github.com/tjdwns335>
2024-01-30 19:09:28 +0900

    reset

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ResetStyle from './ResetStyle'
+
+function App() {
+  return (
+    <>
+      <ResetStyle />
+      <div>App</div>
+    </>
+  )
+}
+
+export default App
