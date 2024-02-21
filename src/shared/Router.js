0b22214d86962d99384c41169a7f2a493fa08169
tjdwns335 <http://github.com/tjdwns335>
2024-02-21 14:08:45 +0900

    (3)

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,14 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "pages/Home";
 import Detail from "pages/Detail";
+import { useState } from "react";
+import Profile from "pages/Profile";
+import Login from "pages/Login";
 
 const Router = () => {
+  const [login, setLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes >
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {login ?
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </> : <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>}
+
       </Routes>
     </BrowserRouter>
   )
